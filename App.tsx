fb8790039d1c22bf6585e4d9d2b36dbfeb64f8d4
Kubalak <kuba.jach@poczta.neostrada.pl>
2021-10-25 21:15:52 +0200

    Testy

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TopBar from './navigation/TopBar';
+// import { configureFonts, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.test}>
+      <TopBar/>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +20,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  test: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:
+  {
+    color: '#f0f',
+    fontWeight: 'bold',
+    fontFamily: 'monospace',
   },
 });
