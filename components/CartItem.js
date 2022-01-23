b22c0fca11bd0f517fc781b08acbb0386bd8ddde
patrykjawor <patryk_rekrutacja@o2.pl>
2022-01-23 09:25:55 +0100

    Poprawa wyglądu CartItem oraz ItemView

--- components/CartItem.js
+++ components/CartItem.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text, Image, Pressable, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
 import { Items } from "../storage/items";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { useNavigation } from '@react-navigation/native';
@@ -8,9 +8,9 @@ import createInstance from "../config";
 
 const axiosInstance = createInstance();
 
-export default function CartItem({ props }) {
+export default function CartItem({ props, reloadFunc}) {
     //TODO: Przekierowanie na widok oferty sprzedaży.
-    
+
     const navigation = useNavigation();
     const [amount, setAmount] = useState(0);
     const [showModal, setShowModal] = useState(false)
@@ -20,10 +20,10 @@ export default function CartItem({ props }) {
 
         axiosInstance.get("/get",
             {
-                params:{
+                params: {
                     key: props.key
                 },
-                
+
             }).then(function (response) { //Uses enviromental variables in the .env file
                 setElement(response.data[0]);
             })
@@ -61,13 +61,12 @@ export default function CartItem({ props }) {
                 break;
             }
         }
-
     }
 
     function DeleteItem(key) {
         Items.cart = listRemove(Items.cart, key)
         setShowModal(false);
-        Items.cart.ForceUpdate
+        reloadFunc(Items.cart)
     }
     /**
      * W przypadku gdy dane się wczytują.
@@ -86,36 +85,39 @@ export default function CartItem({ props }) {
 
     return (
         <View style={style.default}>
+            <View style={style.product}>
+                <Image source={{ uri: element.uri }} style={style.image} />
+            </View>
 
-            <Text style={style.fontSettingsTitle}>
-                {element.title}
-            </Text>
-
-            <Image source={{ uri: element.uri }} style={style.image} />
-
-
-
-            <Text style={style.fontSettingsRest}>
-                <View style={style.Arrows}>
-                    <Pressable onPress={() => DecreaseAmount(element.key)}>
-                        <FontAwesomeIcon icon="arrow-left" size={25} />
-                    </Pressable>
+            <View style={{ flex: 1 }}>
+                <View style={{alignItems:'center', marginBottom: 5}}>
+                    <Text style={style.fontSettingsTitle}>
+                        {element.title}
+                    </Text>
                 </View>
-                Ilość: {props.amount}
-
-                <View style={style.Arrows}>
-                    <Pressable onPress={() => IncreaseAmount(element.key)}>
-                        <FontAwesomeIcon icon="arrow-right" size={25} />
-                    </Pressable>
+                <Text style={style.fontSettingsRest}>
+                    Cena za parę: {element.price} zł
+                </Text>
+                <Text style={style.fontSettingsRest}>
+                    Cena całkowita: {(parseFloat(element.price) * props.amount).toFixed(2)} zł
+                </Text>
+                <View style={style.changeComp}>
+                    <Text style={style.fontSettingsRest}>Ilość:</Text>
+                        <View style={style.changeAmount}>
+                            <TouchableOpacity style={style.touch} onPress={() => IncreaseAmount(element.key)}>
+                                <FontAwesomeIcon icon="caret-up" size={25} />
+                            </TouchableOpacity>
+                            <Text>{props.amount}</Text>
+                            <TouchableOpacity style={style.touch} onPress={() => DecreaseAmount(element.key)}>
+                                <FontAwesomeIcon icon="caret-down" size={25} />
+                            </TouchableOpacity>
+                        </View>
+                        <TouchableOpacity style={style.touch} onPress={() => setShowModal(true)}>
+                            <FontAwesomeIcon icon="trash" size={24} />
+                        </TouchableOpacity>                    
                 </View>
-            </Text>
-
-
+            </View>
             <View style={style.TrashBin}>
-                <Pressable onPress={() => setShowModal(true)}>
-                    <FontAwesomeIcon icon="trash" size={24} />
-                </Pressable>
-
                 <View>
                     <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
                         <Modal.Content maxWidth='400px'>
@@ -139,17 +141,6 @@ export default function CartItem({ props }) {
                 </View>
 
             </View>
-
-
-            <Text style={style.fontSettingsRest}>
-                Cena za pare: {element.price} zł
-            </Text>
-
-            <Text style={style.fontSettingsRest}>
-                Cena całkowita: {(parseFloat(element.price) * props.amount).toFixed(2)} zł
-            </Text>
-
-
         </View>
     );
 
@@ -162,52 +153,56 @@ const style = StyleSheet.create({
 
     default: {
         width: '100%',
-        height: 320,
-        marginBottom: 5,
+        height: 200,
+        marginBottom: 10,
         marginTop: 5,
+        padding: 5,
         elevation: 6,
-        borderRadius: 60,
-        shadowRadius: 10,
-        shadowColor: 'black',
-        shadowOpacity: 10,
-        shadowOffset: { widht: 0, height: 5 },
         backgroundColor: '#F5F5F5',
         alignItems: 'center',
+        flexDirection: 'row'
 
     },
-
+    product: {
+        alignItems: 'center',
+        marginRight: 5,
+    },
     image: {
-        width: 190,
-        height: 190,
-        borderRadius: 70,
+        width: 150,
+        height: 150,
+        borderRadius: 30,
         borderWidth: 1,
         resizeMode: 'stretch',
     },
 
     fontSettingsTitle: {
-        fontSize: 20,
+        fontSize: 18,
         fontWeight: 'bold',
         fontStyle: 'italic',
-
     },
 
     fontSettingsRest:
     {
-        fontSize: 18,
-    },
-    TrashBin:
-    {
-        width: 30,
-        borderRadius: 50,
-        backgroundColor: 'red',
-        alignItems: 'center',
+        fontSize: 16,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     Arrows:
     {
-        marginRight: 20,
-        width: 30,
-        borderRadius: 50,
-        backgroundColor: '#93f600',
         alignItems: 'center',
+    },
+    touch: {
+        width: 20,
+        height: 20,
+    },
+    changeComp: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    changeAmount: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 5,
+        marginRight: 15,
     }
 })
--- screens/ItemView.js
+++ screens/ItemView.js
@@ -1,45 +1,118 @@
 import React from 'react';
-import {View,Text,Image,StyleSheet} from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { useToast } from 'native-base';
+import { Items } from '../storage/items';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import Sizes from '../components/Sizes';
+import Colours from '../components/Colours';
 
-export default function ItemView({route, navigation})
-{
+export default function ItemView({ route, navigation }) {
     props = route.params.props; //Zdobywamy informacje o produkcie który oglądamy
-    return(
-    <View style={style.default}>  
-        <Image source={{uri:props.uri}} style={style.image}/>
-        <Text style={style.titleText}>
-            {props.title}
-        </Text>
+    props.price = parseFloat(props.price)
+    const toast = useToast();
+    function AddToCart(){
         
-    </View>
+        let item = null;
+        if(Items.cart.length > 0)
+        {
+            
+            for (var i = 0; i < Items.cart.length; i++)
+            {
+                if(Items.cart[i].key !== undefined && Items.cart[i].key === props.key)
+                {
+                    Items.cart[i].amount++;
+                    item = props.title;
+                    break;
+                }
+                else if(i === Items.cart.length - 1)
+                {
+                    Items.cart.push({key: props.key, amount: 1});
+                    item = props.title;
+                }
+            }
+        }
+        else
+        {
+            Items.cart.push({key: props.key, amount: 1});
+            item = props.title;
+        }
+
+        toast.show({
+        title: "Nowy zakup",
+        status: "success",
+        description: item +"\nDodano element do koszyka",
+        duration: 1500
+        })
+    }
+    return (
+        <View style={style.default}>
+            <View style={style.header}>
+                <Image source={{ uri: props.uri }} style={style.image} />
+                <Text style={style.titleText}>
+                    {props.title}
+                </Text>
+            </View>
+            <View style={style.info}>
+                <View style={{ alignItems: 'center' }}><Text style={style.infoTitle}>Informacje o produkcie:</Text></View>
+                <View style={{ paddingLeft: 25 }}>
+                    <Text style={style.infoText}>Materiał wykonania: <Text style={{ fontWeight: 'bold' }}>{props.material}</Text></Text>
+                    <Text style={style.infoText}>Dostępne rozmiary: <Sizes props={JSON.parse(props.availableSizes)} /></Text>
+                    <Text style={style.infoText}>Dostępne kolory: <Colours props={JSON.parse(props.availableColours)} /></Text>
+                    <Text style={style.infoText}>Cena za parę: <Text style={{ fontWeight: 'bold' }}>{props.price.toFixed(2).toString().replace('.', ',')} zł</Text></Text>
+                </View>
+            </View>
+            <TouchableOpacity style={style.addToCart} onPress={() => AddToCart()}><Text style={{fontSize: 16}}>Dodaj do koszyka <FontAwesomeIcon icon="cart-plus" size={18}/></Text></TouchableOpacity>
+
+        </View>
     );
 }
 
 const style = StyleSheet.create({
 
-    default:{
-        width:'100%',
-        height:300,
-        marginTop:5,
-        alignItems:'center',
-        backgroundColor:'#F5F5F5',
-        flex:1,      
+    default: {
+        width: '100%',
+        height: 300,
+        padding: 10,
+        backgroundColor: '#FFFFFF',
+        flex: 1,
     },
-
-    image:{
-        width: 300,
-        height: 210,
-        borderRadius:25,
+    header: {
+        alignItems: 'center',
+    },
+    image: {
+        width: 250,
+        height: 250,
+        borderRadius: 25,
         overflow: 'hidden',
-        shadowColor: '#202020',
-        shadowOffset: {width: 0, height: 0},
-        shadowRadius: 5, 
+        marginBottom: 5,
     },
 
-    titleText:{
-        fontSize:20,
-        fontStyle:'italic',
-        fontWeight:'bold',
-        
+    titleText: {
+        fontSize: 20,
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+
+    },
+    info: {
+        marginTop: 20,
+    },
+    infoTitle: {
+        fontSize: 18,
+    },
+    infoText: {
+        fontSize: 16,
+        lineHeight: 25,
+        marginTop: 5,
+    },
+    addToCart:{
+        width: 200,
+        height: 50,
+        elevation: 2,
+        backgroundColor: '#40F98F',
+        borderRadius: 12,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 35,
     }
 })
