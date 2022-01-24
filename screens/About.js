8d273bda3482b416d8f17465af4ae4b3c990a101
Kubalak <kuba.jach@poczta.neostrada.pl>
2022-01-24 18:41:26 +0100

    Drobne poprawki.

--- screens/About.js
+++ screens/About.js
@@ -1,68 +1,71 @@
 import React from "react";
-import { View, Text, StyleSheet, ImageBackground, Image} from "react-native";
+import { View, Text, StyleSheet, ImageBackground, Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 
 const BottomTabNavigator = createBottomTabNavigator();
 
-function Kuba(){
-    return(
+function Kuba() {
+    return (
         <View style={style.default}>
-            <ImageBackground source={require('../assets/background.png')} style={{flex:1, justifyContent: 'center', alignItems: 'center' }}>
-            <Image source={require('../assets/icon.png')} style={{ width: 200, height: 200, resizeMode: 'stretch', marginBottom: 25 }} />
-            <View style={style.styleKuba}>
-            <Text style={style.KubaHeader}><FontAwesomeIcon icon="fire" size={30} color='#EA9935'/>Kuba<FontAwesomeIcon icon="fire" size={30} color='#EA9935'/></Text>
-            <Text style={style.KubaText}>
-                - Stworzenie nawigacji{'\n'}
-                - Obsługa axiosa{'\n'}
-                - Backend w <Text style={{...style.KubaText, fontStyle: 'italic'}}>PHP, MySQL <FontAwesomeIcon icon="grin-tongue-squint" size={25} color='#EA9935'/></Text>{'\n'}
-                - Obsługa aparatu{'\n'}
-                - Logowanie do aplikacji {'\n'}
-                - Przesyłanie formularzy{'\n'}
-            </Text>
-            </View>
+
+            <ImageBackground source={require('../assets/background.png')} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={style.styl_Patryk_title}>  Butto shoppo INC   </Text>
+                <Image source={require('../assets/icon.png')} style={{ width: 200, height: 200, resizeMode: 'stretch', marginBottom: 25 }} />
+                <View style={style.styleKuba}>
+                    <Text style={style.KubaHeader}><FontAwesomeIcon icon="fire" size={30} color='#EA9935' />Kuba<FontAwesomeIcon icon="fire" size={30} color='#EA9935' /></Text>
+                    <Text style={style.KubaText}>
+                        - Stworzenie nawigacji{'\n'}
+                        - Obsługa axiosa{'\n'}
+                        - Backend w <Text style={{ ...style.KubaText, fontStyle: 'italic' }}>PHP</Text> i <Text style={{ ...style.KubaText, fontStyle: 'italic' }}>MySQL <FontAwesomeIcon icon="grin-tongue-squint" size={25} color='#EA9935' /></Text>{'\n'}
+                        - Obsługa aparatu{'\n'}
+                        - Logowanie do aplikacji {'\n'}
+                        - Przesyłanie formularzy{'\n'}
+                        - Dodanie ikon{'\n'}
+                        - Logo aplikacji
+                    </Text>
+                </View>
             </ImageBackground>
         </View>
     );
 }
 
-function Patryk(){
-    return(
+function Patryk() {
+    return (
         <View style={style.default}>
-            <ImageBackground source={require('../assets/background.png')} style={{flex:1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={style.styl_Patryk_title}>  Butto shoppo INC   </Text>
-            <Image source={require('../assets/icon.png')} style={{ width: 200, height: 200, resizeMode: 'stretch', marginBottom: 25 }} />
-            <Text style={style.styl_Patryk}>
-                *Patryk* {"\n"}
-                - Interfejs koszyka {"\n"}
-                - Item View {"\n"}
-                - Toast przy zakupie {"\n"}
-                - Modal przy usuwaniu {"\n"}
-            </Text>
+            <ImageBackground source={require('../assets/background.png')} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={style.styl_Patryk_title}>  Butto shoppo INC   </Text>
+                <Image source={require('../assets/icon.png')} style={{ width: 200, height: 200, resizeMode: 'stretch', marginBottom: 25 }} />
+                <Text style={style.styl_Patryk}>
+                    *Patryk* {"\n"}
+                    - Interfejs koszyka {"\n"}
+                    - Item View {"\n"}
+                    - Toast przy zakupie {"\n"}
+                    - Modal przy usuwaniu {"\n"}
+                </Text>
             </ImageBackground>
         </View>
     );
 }
 
-export default function About()
-{
-    return(
+export default function About() {
+    return (
         <BottomTabNavigator.Navigator>
             <BottomTabNavigator.Screen
-            options={{
-                headerShown: false,
-                tabBarIcon: (focused) => <FontAwesomeIcon icon="dizzy" size={18} color='#FFA538'/> //Należy zmienić ikonę
-            }}
-            name="Kuba"
-            component={Kuba}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => <FontAwesomeIcon icon="dizzy" size={18} color={focused ? '#5291FF' : '#FFA538'} /> //Należy zmienić ikonę
+                }}
+                name="Kuba"
+                component={Kuba}
             />
             <BottomTabNavigator.Screen
-            options={{
-                headerShown: false,
-                tabBarIcon: (focused) => <FontAwesomeIcon icon="tired" size={18} color="#FFA538"/> //Należy zmienić ikonę
-            }}
-            name="Patryk"
-            component={Patryk}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => <FontAwesomeIcon icon="tired" size={18} color={focused ? '#5291FF' : '#FFA538'} /> //Należy zmienić ikonę
+                }}
+                name="Patryk"
+                component={Patryk}
             />
 
         </BottomTabNavigator.Navigator>
@@ -70,44 +73,45 @@ export default function About()
 }
 
 const style = StyleSheet.create({
-    default:{
-        flex:1,
+    default: {
+        flex: 1,
         backgroundColor: 'white',
     },
-    styl_Patryk:{
+    styl_Patryk: {
         fontSize: 30,
         fontWeight: 'bold',
-        fontStyle:'italic',
+        fontStyle: 'italic',
         textAlignVertical: 'center',
         textAlign: 'center',
         color: '#EA9935',
     },
-    styleKuba:{
+    styleKuba: {
         width: '100%',
-        
+
     },
-    KubaHeader:{
+    KubaHeader: {
         color: '#EA9935',
         fontSize: 30,
         fontWeight: 'bold',
         textAlign: 'center'
     },
-    KubaText:{
+    KubaText: {
         fontSize: 25,
         marginLeft: 25,
         fontWeight: 'bold',
-        color:'#EA9935',
+        color: '#EA9935',
 
         color: '#FF8C00',
     },
-    styl_Patryk_title:{
+    styl_Patryk_title: {
         fontSize: 40,
+        marginBottom: 5,
         fontWeight: 'bold',
-        fontStyle:'italic',
+        fontStyle: 'italic',
         textAlignVertical: 'center',
         textAlign: 'center',
         color: '#FF8C00',
-        textDecorationLine:'underline',
-        
+        textDecorationLine: 'underline',
+
     }
 })
